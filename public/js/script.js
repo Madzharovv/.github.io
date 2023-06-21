cac8bc583d504888531116e007e5107cd7925a00
“first_name <71776566+Madzharovv@users.noreply.github.com>
2023-06-21 17:17:32 +0100

    Updated website portfolio

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,28 @@
-let sendButton = document.getElementById('button');
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.value = 'Sending...';
 
-   const serviceID = 'service_t67ua0t';
-   const templateID = 'template_g8od3gm';
+    const serviceID = 'service_t67ua0t';
+    const templateID = 'template_g8od3gm';
 
-   emailjs.sendForm(serviceID, templateID, form)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
+    emailjs.sendForm(serviceID, templateID, form)
+      .then(() => {
+        btn.innerHTML = 'Sending....';
+      
 
-
-
-
+        setTimeout(function() {
+          btn.innerHTML = 'Message Sent';
+          setTimeout(function() {
+            btn.innerHTML = 'Send Again';
+          }, 2000);
+        }, 2000);
+      })
+      .catch((err) => {
+       btn.innerHTML="Error"
+        alert(JSON.stringify(err));
+      });
+  });
