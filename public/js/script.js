6c678e2d1a1bc9d9aa8bf5eb00baaa358093752d
“first_name <71776566+Madzharovv@users.noreply.github.com>
2023-06-21 17:50:29 +0100

    Bug Fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('button');
+const sendForm = document.getElementById('form');
 
-document.getElementById('form')
+sendForm
   .addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -12,17 +13,22 @@ document.getElementById('form')
     emailjs.sendForm(serviceID, templateID, form)
       .then(() => {
         btn.innerHTML = 'Sending....';
-      
+        sendForm.reset();
 
         setTimeout(function() {
           btn.innerHTML = 'Message Sent';
           setTimeout(function() {
             btn.innerHTML = 'Send Again';
+            
           }, 2000);
         }, 2000);
+     
       })
+
       .catch((err) => {
        btn.innerHTML="Error"
         alert(JSON.stringify(err));
       });
   });
+
+  
